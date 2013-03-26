4209c0502718e76952215490d623f6e8053545a3
Vhornets <vhornets@gmail.com>
2013-03-26 14:52:36 +0200

    Updated

--- MessageBox.js
+++ MessageBox.js
@@ -1,24 +1,17 @@
 var MessageBox = (function () {
-    function MessageBox(obj) {
+    function MessageBox() {
         this.delta = new Array();
-        $(obj).show();
-        $(obj).offset({
-            top: ($(document).height() / 2) - ($(obj).height() / 2),
-            left: ($(document).width() / 2) - ($(obj).width() / 2)
-        });
-        $(document.body).prepend("<div id='message-block-bg'></div>");
-        $("#message-block-bg").show();
-        this.initDragObject(obj);
+        this.createBox();
+        this.initDragObject(".message-block-header");
         this.close();
     }
     MessageBox.prototype.initDragObject = function (obj) {
         var _this = this;
         $(obj).mousedown(function (e) {
-            _this.dragObject = $(obj);
+            _this.dragObject = $(obj).parent();
             _this.delta['X'] = e.clientX - $(obj).offset().left;
             _this.delta['Y'] = e.clientY - $(obj).offset().top;
             _this.dragObject.css({
-                'position': 'absolute',
                 'opacity': '0.5'
             });
             _this.startDrag();
@@ -34,8 +27,6 @@ var MessageBox = (function () {
             });
             $(document).unbind('mousemove');
             _this.dragObject = null;
-            _this.deltaX = null;
-            _this.deltaY = null;
         });
     };
     MessageBox.prototype.startDrag = function () {
@@ -49,8 +40,18 @@ var MessageBox = (function () {
     };
     MessageBox.prototype.close = function () {
         $(".message-block-close").click(function () {
-            $(this).parent().parent().hide();
-            $("#message-block-bg").hide();
+            $(".message-block").remove();
+            $("#message-block-bg").remove();
+        });
+    };
+    MessageBox.prototype.createBox = function () {
+        $(document.body).prepend("<div id='message-block-bg'></div>");
+        $("#message-block-bg").after("<div class='message-block'>" + "<div class='message-block-header'>" + "<span class='message-block-header-text'></span><span class='message-block-close'>X</span>" + "</div>" + "<div class='message-block-body'></div>" + "</div>");
+        $("#message-block-bg").show();
+        $(".message-block").show();
+        $(".message-block").offset({
+            top: ($(document).height() / 2) - ($(".message-block").height() / 2),
+            left: ($(document).width() / 2) - ($(".message-block").width() / 2)
         });
     };
     MessageBox.prototype.setHeaderText = function (text) {
--- MessageBox.ts
+++ MessageBox.ts
@@ -3,31 +3,25 @@ class MessageBox {
     private delta = new Array(); //смещение курсора для плавного перетаскивания
     private headerText:string;
     private bodyText:string;
-
     /*
     Конструктор получает jQuery-селектор
     показывает скрытый контейнер и позиционирует его по центру экрана
      */
-    constructor(obj) {
-        $(obj).show();
-        $(obj).offset({top: ($(document).height() / 2) - ($(obj).height() / 2), left: ($(document).width() / 2) - ($(obj).width() / 2)});
-        $(document.body).prepend("<div id='message-block-bg'></div>");
-        $("#message-block-bg").show();
-
-        this.initDragObject(obj);
+    constructor() {
+        this.createBox();
+        this.initDragObject(".message-block-header");
         this.close();
     }
-
     /*
     Инициализация объекта перетаскивания,
     который записывается в this.DragObject
     */
     private initDragObject(obj):void {
         $(obj).mousedown((e) => {
-            this.dragObject = $(obj);
+            this.dragObject = $(obj).parent();
             this.delta['X']= e.clientX - $(obj).offset().left;
             this.delta['Y'] = e.clientY - $(obj).offset().top;
-            this.dragObject.css({'position':'absolute', 'opacity':'0.5'});
+            this.dragObject.css({'opacity':'0.5'});
             this.startDrag();
             return false;
         });
@@ -39,8 +33,6 @@ class MessageBox {
             $(this.dragObject).css({'opacity':'1'});
             $(document).unbind('mousemove');
             this.dragObject = null;
-            this.deltaX = null;
-            this.deltaY = null;
         });
     }
 
@@ -52,9 +44,23 @@ class MessageBox {
 
     private close():void {
         $(".message-block-close").click(function() {
-            $(this).parent().parent().hide();
-            $("#message-block-bg").hide();
+            $(".message-block").remove();
+            $("#message-block-bg").remove();
         });
+    }
+
+    private createBox():void {
+        $(document.body).prepend("<div id='message-block-bg'></div>");
+        $("#message-block-bg").after(
+            "<div class='message-block'>" +
+            "<div class='message-block-header'>" +
+            "<span class='message-block-header-text'></span><span class='message-block-close'>X</span>" +
+            "</div>" +
+            "<div class='message-block-body'></div>" +
+            "</div>");
+        $("#message-block-bg").show();
+        $(".message-block").show();
+        $(".message-block").offset({top: ($(document).height() / 2) - ($(".message-block").height() / 2), left: ($(document).width() / 2) - ($(".message-block").width() / 2)});
     }
 
     public setHeaderText(text:string) {
