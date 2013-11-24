7eae2b3e3d61a24f5b6b3e118466c15ae440fca2
Vhornets <vhornets@gmail.com>
2013-11-24 19:12:48 +0200

    Create page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,33 @@
-console.log('This would be the main JS file.');
+window.onload = function() {
+    var cl = document.getElementById("cl");
+    cl.onclick = function() {
+        new MessageBox({
+            header: 'Hello',
+            body: '2.html',
+            loadAJAX: true,
+            draggable: true
+        });
+    }
+
+    var images = document.getElementsByTagName('img');
+    for(var i = 0; i<images.length; i++) {
+        images[i].onclick = function() {
+           new MessageBox({
+                header: this.getAttribute('alt'),
+                body: "<img src='" + this.src + "' width='300' />",
+                loadAJAX: false,
+                draggable: true
+            });
+        }
+    }
+
+    document.getElementById('gallery').onclick = function() {
+        new MessageBox({
+                header: this.getAttribute('alt'),
+                body: '',
+                loadAJAX: false,
+                draggable: true          
+        })
+		.initGallery('MBGallery');
+    }
+}
